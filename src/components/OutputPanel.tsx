6b83bdefcc7c42bfdd552490e584264a7223ffb7
omar <o.gamal.abdulzaher@gmail.com>
2022-11-29 01:50:18 +0200

    implement calculator logic

--- src/components/OutputPanel.tsx
+++ src/components/OutputPanel.tsx
@@ -1,13 +1,35 @@
 import OutputPiece from './OutputPiece';
 
-const OutputPanel = () => {
+interface OutputPanelProps {
+  setBill: React.Dispatch<React.SetStateAction<number>>;
+  setTip: React.Dispatch<React.SetStateAction<number>>;
+  setNumberOfPeople: React.Dispatch<React.SetStateAction<number>>;
+  tipAmount: number;
+  total: number;
+}
+
+const OutputPanel = ({
+  setBill,
+  setTip,
+  setNumberOfPeople,
+  tipAmount,
+  total,
+}: OutputPanelProps) => {
+  const handleReset = () => {
+    setBill(NaN);
+    setTip(NaN);
+    setNumberOfPeople(1);
+  };
+
   return (
     <section className='output-panel panel'>
       <div>
-        <OutputPiece title='Tip Amount' tag='/ person' price={0.0} />
-        <OutputPiece title='Total' tag='/ person' price={0.0} />
+        <OutputPiece title='Tip Amount' tag='/ person' price={tipAmount} />
+        <OutputPiece title='Total' tag='/ person' price={total} />
       </div>
-      <button className='reset-button'>Reset</button>
+      <button className='reset-button' onClick={handleReset}>
+        Reset
+      </button>
     </section>
   );
 };
